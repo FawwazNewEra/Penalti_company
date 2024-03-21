12f79f2743e249ecd3ba04ed3084ed8eae74cecf
Fawwaz129 <wazwazzsrhrr@gmail.com>
2024-03-21 16:07:36 +0700

    fix

--- E-Commerce/resources/views/welcome.blade.php
+++ E-Commerce/resources/views/welcome.blade.php
@@ -136,29 +136,29 @@
                   @csrf
                   <div class="w-full gap-3 flex">
                     <div>
-                    <label class="block mb-22 text-sm font-medium text-white" for="firstname">First Name</label>
-                    <input type="text" name="firstname" id="firstname" class="bg-[#464646] border border-[#464646] text-gray-900 text-sm block w-full p-2.5" placeholder="First Name">
+                    <label for="firstname" class="block mb-2 text-sm font-medium text-white">First Name</label>
+                    <input type="text" name="firstname" id="firstname" class="bg-[#464646] border border-[#464646] text-gray-900 text-sm block w-full p-2" placeholder="First Name">
                     </div>
                     <div>
-                    <label class="block mb-22 text-sm font-medium text-white" for="lastname">Last Name</label>
-                    <input type="text" name="lastname" id="lastname" class="bg-[#464646] border border-[#464646] text-gray-900 text-sm block w-full p-2.5" placeholder="Last Name">
+                    <label for="lastname" class="block mb-2 text-sm font-medium text-white">Last Name</label>
+                    <input type="text" name="lastname" id="lastname" class="bg-[#464646] border border-[#464646] text-gray-900 text-sm block w-full p-2" placeholder="Last Name">
                     </div>
                   </div>
                     <div>
                         <label for="email" class="block mb-2 text-sm font-medium text-white">Email Address</label>
-                        <input type="email" name="email" id="email" class="bg-[#464646] border border-[#464646] text-gray-900 text-sm block w-full p-2.5" placeholder="Your Email" required />
+                        <input type="email" name="email" id="email" class="bg-[#464646] border border-[#464646] text-gray-900 text-sm block w-full p-2" placeholder="Your Email" required />
                     </div>
                     <div>
                       <label class="block mb-2 text-sm font-medium text-white" for="number">Mobile Number</label>
-                      <input type="number" name="number" id="number" class="bg-[#464646] border border-[#464646] text-gray-900 text-sm block w-full p-2.5" placeholder="Your Email" required />
+                      <input type="number" name="number" id="number" class="bg-[#464646] border border-[#464646] text-gray-900 text-sm block w-full p-2" placeholder="Your Email" required />
                     </div>
                     <div>
                         <label for="password" class="block mb-2 text-sm font-medium text-white">Password</label>
-                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-[#464646] border border-[#464646] text-gray-900 text-sm block w-full p-2.5 " required />
+                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-[#464646] border border-[#464646] text-gray-900 text-sm block w-full p-2 " required />
                     </div>
                     <div>
                         <label for="password" class="block mb-2 text-sm font-medium text-white">Verifikasi Password</label>
-                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-[#464646] border border-[#464646] text-gray-900 text-sm block w-full p-2.5 " required />
+                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-[#464646] border border-[#464646] text-gray-900 text-sm block w-full p-2 " required />
                     </div>
                     <div class="w-full text-center">
                     <button type="submit" class=" bg-white  font-medium rounded-sm text-sm px-5 py-2 text-center">Create an account</button>
