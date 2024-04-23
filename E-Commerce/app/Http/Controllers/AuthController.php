bc21d38fff820264bab715184ec4bb450b633f86
ones <almanafyounes@gmail.com>
2024-04-23 16:02:30 +0300

    haihihiha

--- E-Commerce/app/Http/Controllers/AuthController.php
+++ E-Commerce/app/Http/Controllers/AuthController.php
@@ -45,7 +45,7 @@ class AuthController extends Controller
             'password' => Hash::make($request->input('password'))
         ]);
     
-        return redirect()->route('welcome'); // Assuming you have a route named 'welcome' for your homepage
+        return redirect('/'); // Assuming you have a route named 'welcome' for your homepage
     }
     
 }
